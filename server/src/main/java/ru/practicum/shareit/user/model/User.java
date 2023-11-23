992595bc1e65c6ae2714bef22948d6e830e20580
DaniilStamatov <108268346+DaniilStamatov@users.noreply.github.com>
2023-11-23 14:27:51 +0300

    commit for gitHub

---
+++ server/src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.user.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
